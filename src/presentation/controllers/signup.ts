a4bc885dae68dd29b08c1f1961c521b44752141d
EmersonBraun <emersonfbraun@gmail.com>
2020-05-15 17:38:48 -0300

    feat: ensure SingUpController return 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,6 +17,7 @@ export class SingUpController implements Controller {
         }
       }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
       if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
       return serverError()
